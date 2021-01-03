a96fdaa7bb4022894c43323653b18f2295e60fc1
Alejandro Lobato Molina <a.lobato.molina@gmail.com>
2021-01-02 23:46:01 -0600

    Day 2 - Complete function and read input data.

--- TempCalculator/logic.js
+++ TempCalculator/logic.js
@@ -1,5 +1,13 @@
-function celsiusToFahrenheit(celsius)
+function celsiusToFahrenheit()
 { 
-    alert("Pressed!");
-    return celsius * 1.8 + 32 
+    let celsius = document.getElementById("celsiusInput").value;
+    if(celsius == "")
+    {
+        alert("Please enter a number.");  
+    }
+    else
+    {
+        result = celsius * 1.8 + 32;
+        document.getElementById("resultText").textContent = result;
+    } 
 }
